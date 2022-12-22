f3ff16fd24901476388fec93a26e361a838a3e86
samiulislam09 <samiulislamsawon09@gmail.com>
2022-12-23 01:51:39 +0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ErrorPage from "./pages/ErrorPage";
+import ProductsPage from "./pages/ProductsPage";
+import SingleProductPage from "./pages/SingleProductPage";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/checkout" element={<CheckoutPage />}></Route>
+        <Route path="/products" element={<ProductsPage />}></Route>
+        <Route path="/singleProduct" element={<SingleProductPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;

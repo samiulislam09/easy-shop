3503cb63e7015af6ce62335ca18f59d299319057
samiulislam09 <samiulislamsawon09@gmail.com>
2022-12-23 02:32:29 +0600

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { Wrapper } from "../styles/Footer.styles";
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return (
+    <div>
+      <Wrapper>
+        <h5>Copyright &copy; easy-shop {new Date().getFullYear()} </h5>
+      </Wrapper>
+    </div>
+  );
+};
 
-const Wrapper = styled.footer`
-  height: 5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: var(--clr-black);
-  text-align: center;
-  span {
-    color: var(--clr-primary-5);
-  }
-  h5 {
-    color: var(--clr-white);
-    margin: 0.1rem;
-
-    font-weight: 400;
-    text-transform: none;
-    line-height: 1.25;
-  }
-  @media (min-width: 776px) {
-    flex-direction: row;
-  }
-`
-
-export default Footer
+export default Footer;
